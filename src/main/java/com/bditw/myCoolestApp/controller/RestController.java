a6f36e0e5dbe2141d3644f491f1438fa27634b88
t-r-a-c-y <tracytesi69@gmail.com>
2025-05-07 13:39:19 +0200

    writing a string

--- src/main/java/com/bditw/myCoolestApp/controller/RestController.java
+++ src/main/java/com/bditw/myCoolestApp/controller/RestController.java
@@ -3,12 +3,22 @@ package com.bditw.myCoolestApp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class RestController {
+//    @GetMapping("/helloW")
+//    public String helloWorld(Model model){
+//        model.addAttribute("theDate",java.time.LocalDate.now());
+//        return "helloWorld";
+//    }
+//}
+
 
 @Controller
 public class RestController {
-    @GetMapping("/helloW")
-    public String helloWorld(Model model){
-        model.addAttribute("theDate",java.time.LocalDate.now());
+    @GetMapping("/helloWor")
+    public String helloWorld(Model model) {
+        model.addAttribute("message","This is the first application");
         return "helloWorld";
     }
 }
