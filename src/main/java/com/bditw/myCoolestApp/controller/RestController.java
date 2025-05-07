dfb71208b124692541f25b295924315a9d15cc3d
t-r-a-c-y <tracytesi69@gmail.com>
2025-05-07 13:58:47 +0200

    submitting the form

--- src/main/java/com/bditw/myCoolestApp/controller/RestController.java
+++ src/main/java/com/bditw/myCoolestApp/controller/RestController.java
@@ -3,22 +3,23 @@ package com.bditw.myCoolestApp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class RestController {
-//    @GetMapping("/helloW")
-//    public String helloWorld(Model model){
-//        model.addAttribute("theDate",java.time.LocalDate.now());
-//        return "helloWorld";
-//    }
-//}
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RestController {
-    @GetMapping("/helloWor")
-    public String helloWorld(Model model) {
-        model.addAttribute("message","This is the first application");
+    @GetMapping("/helloW")
+    public String helloWorld(Model model){
+        model.addAttribute("theDate",java.time.LocalDate.now());
+        return "helloWorld";
+    }
+    @RequestMapping("/showForm")
+    public String showForm(Model model){
+        return "nameForm";
+    }
+    @RequestMapping("/proccessForm")
+    public String proccessForm(){
         return "helloWorld";
     }
 }
+
+
